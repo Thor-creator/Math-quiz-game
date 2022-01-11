926c6699c5e6281b6299b375e182223b60189c7b
Thor-creator <69631224+Thor-creator@users.noreply.github.com>
2022-01-11 20:36:19 +0530

    Add files via upload

---
+++ quiz_game_page.js
@@ -0,0 +1,7 @@
+function send(){
+   number1 = document.getElementById("number1_input").value
+   number2 = document.getElementById("number2_input").value 
+   document.getElementById("question").innerHTML = number1+"x"+number2 
+  
+     
+}
