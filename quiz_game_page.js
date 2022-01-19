1d8f57281943497dee869d9e1a9700c6fcad264f
Thor-creator <69631224+Thor-creator@users.noreply.github.com>
2022-01-19 14:22:12 +0530

    Add files via upload

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -1,16 +1,4 @@
-player1_name = localStorage.getItem("player1_name");
-player2_name = localStorage.getItem("player2_name");
 
-player1_score = 0;
-player2_score = 0;
-
-document.getElementById("player_question").innerHTML = "Question turn: " + player1_name
-document.getElementById("player_answer").innerHTML = "Answer turn: " + player2_name
-document.getElementById("player1_name").innerHTML = player1_name + ": "
-document.getElementById("player2_name").innerHTML = player2_name + ": "
-
-document.getElementById("player1_score").innerHTML = player1_score
-document.getElementById("player2_score").innerHTML = player2_score
 
 function send(){
    number1 = document.getElementById("number1").value
